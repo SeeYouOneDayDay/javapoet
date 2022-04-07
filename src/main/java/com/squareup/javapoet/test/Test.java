8527184750e3f37fc2a48df08e1e8893fc5fedc4
sanbo <sanbo.xyz@gmail.com>
2022-04-07 14:37:29 +0800

    add case

---
+++ src/main/java/com/squareup/javapoet/test/Test.java
@@ -0,0 +1,66 @@
+package com.squareup.javapoet.test;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import javax.lang.model.element.Modifier;
+import java.io.IOException;
+
+/**
+ * @Copyright © 2022 sanbo Inc. All rights reserved.
+ * @Description: TODO
+ * @Version: 1.0
+ * @Create: 2022/4/7 2:29 PM
+ * @author: sanbo
+ */
+public class Test {
+    public static void main(String[] args) throws IOException {
+//        newHellioWorldClassAndMain();
+        
+        xx();
+    }
+
+    private static void xx() throws IOException {
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(whatsMyName("slimShady"))
+                .addMethod(whatsMyName("eminem"))
+                .addMethod(whatsMyName("marshallMathers"))
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+                .build();
+
+        javaFile.writeTo(System.out);
+    }
+
+    private static MethodSpec whatsMyName(String name) {
+        return MethodSpec.methodBuilder(name)
+                .returns(String.class)
+                .addStatement("return $S", name)
+                .build();
+    }
+
+    private static void newHellioWorldClassAndMain() {
+        MethodSpec main = MethodSpec.methodBuilder("main")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(void.class)
+                .addParameter(String[].class, "args")
+                .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+                .build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+                .build();
+
+        // one print method
+//        javaFile.writeTo(System.out);
+        // other print
+        System.out.println(javaFile.toString());
+    }
+}
